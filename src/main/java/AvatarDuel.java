5e1b10116a0ad07e5c118b2a4dfcc4097965fae1
Yonas Adiel <yonazadielwiguna@gmail.com>
2020-03-26 10:18:23 +0700

    Add land csv file and reader

--- src/main/java/AvatarDuel.java
+++ src/main/java/AvatarDuel.java
@@ -1,17 +1,37 @@
 package com.avatarduel;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import com.avatarduel.model.Element;
+import com.avatarduel.model.Land;
+import com.avatarduel.util.CSVReader;
+
 public class AvatarDuel extends Application {
+  private static final String LAND_CSV_FILE_PATH = "src/res/card/data/land.csv";
+
+  public void loadCards() throws IOException {
+    File landCSVFile = new File(LAND_CSV_FILE_PATH);
+    CSVReader landReader = new CSVReader(landCSVFile, ",");
+    landReader.setSkipHeader(true);
+    List<String[]> landRows = landReader.read();
+    for (String[] row : landRows) {
+      System.out.println(row[1]);
+      Land l = new Land(row[0], row[2], Element.valueOf(row[1]));
+    }
+  }
 
   @Override
   public void start(Stage stage) {
     Text text = new Text();
-    text.setText("Avatar Duel!");
+    text.setText("Loading...");
     text.setX(50);
     text.setY(50);
 
@@ -23,6 +43,13 @@ public class AvatarDuel extends Application {
     stage.setTitle("Avatar Duel");
     stage.setScene(scene);
     stage.show();
+
+    try {
+      this.loadCards();
+      text.setText("Avatar Duel!");
+    } catch (IOException e) {
+      text.setText("Failed to load cards: " + e);
+    }
   }
 
   public static void main(String[] args) {
